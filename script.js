4aa6c8df5d1670a9c8f2732c82828522a8e3b244
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-08-09 14:32:14 +0545

    Add initial HTML and JavaScript files for JS demo

---
+++ script.js
@@ -0,0 +1,32 @@
+// Basics of JS
+console.log("Hello, World!")
+
+//Variables
+var flName = "Bishal Rijal"
+console.log(flName)
+
+const name1 = "John"
+const ageOfName1=15
+const professionOfName1 = "Developer"
+
+const name2 = "Doe"
+const name3 = "Smith"
+
+console.log(name1, name2, name3)
+
+// Arrays
+const names = ["John", "Doe", "Smith"]
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+
+// Objects
+const person = {
+    name: 'John',
+    age: 15,
+    profession: "Developer"
+}
+
+console.log(person.name)
+console.log(person.age)
+console.log(person.profession)
